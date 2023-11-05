dc193e1f08b76a3b2e0edcf7a4f74c30968ba28c
Omared250 <79874354+Omared250@users.noreply.github.com>
2023-11-05 22:53:07 +0100

    form to capture the logs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { NavBar } from './components/NavBar'
+import { SendLogOption1 } from './components/SendLogOption1'
 
 function App() {
 
   return (
     <div>
       <NavBar />
+      <SendLogOption1 />
     </div>
   )
 }
