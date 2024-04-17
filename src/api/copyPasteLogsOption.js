3383ff3e16f06ea2c101a6397dfcdddd7169c4a2
Omared250 <79874354+Omared250@users.noreply.github.com>
2024-04-17 16:18:19 +0200

    including Api Key in copy/paste send logs option

--- src/api/copyPasteLogsOption.js
+++ src/api/copyPasteLogsOption.js
@@ -1,12 +1,16 @@
 import axios from "axios"
 import { getEnvVariables } from '../helpers/getEnvVariables';
 
-const { VITE_API_URL_COPYPASTE_LOGS } = getEnvVariables();
+const { VITE_API_URL_COPYPASTE_LOGS, VITE_API_GETWAY_KEY } = getEnvVariables();
 const apiUrl = VITE_API_URL_COPYPASTE_LOGS;
 
 export const copyPasteLogsOption = async (logValue) => {
     try {
-        const { data } = await axios.post(apiUrl, logValue);
+        const { data } = await axios.post(apiUrl, logValue, {
+            headers: {
+                'x-api-key': VITE_API_GETWAY_KEY,
+            }
+        });
         console.log({
             status: data.statusCode,
             msg: 'Log sent to New Relic'
