0310012a962a22d5aef2e758b76eba943ee7fa42
Omared250 <79874354+Omared250@users.noreply.github.com>
2024-04-17 16:05:56 +0200

    adding env varibale for Apigetway Key

--- src/api/newRelicLogs.js
+++ src/api/newRelicLogs.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 import { getEnvVariables } from '../helpers/getEnvVariables';
 
-const { VITE_API_URL_CUSTOM_LOGS } = getEnvVariables();
+const { VITE_API_URL_CUSTOM_LOGS, VITE_API_GETWAY_KEY } = getEnvVariables();
 const apiGatewayUrl = VITE_API_URL_CUSTOM_LOGS;
 
 export const newReliclogs = async (logMessage) => {
 
+    console.log(JSON.stringify(logMessage));
+
     try {
-        const { data } = await axios.post(apiGatewayUrl, logMessage);
+        const { data } = await axios.post(apiGatewayUrl, logMessage, {
+            headers: {
+                'x-api-key': VITE_API_GETWAY_KEY,
+            }
+        });
         console.log({
             status: data.statusCode,
             msg: 'Log sent to New Relic'
--- src/helpers/getEnvVariables.js
+++ src/helpers/getEnvVariables.js
@@ -7,6 +7,7 @@ export const getEnvVariables = () => {
         // ...import.meta.env
         VITE_API_URL_CUSTOM_LOGS: import.meta.env.VITE_API_URL_CUSTOM_LOGS,
         VITE_API_URL_COPYPASTE_LOGS: import.meta.env.VITE_API_URL_COPYPASTE_LOGS,
+        VITE_API_GETWAY_KEY: import.meta.env.VITE_API_GETWAY_KEY,
     }
 
 };
